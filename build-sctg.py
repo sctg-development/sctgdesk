79ee2704d2fb6367b262319d635585e2d75415cf
Ronan <ronan.le_meillat@highcanfly.club>
2024-04-07 18:48:22 +0200

    Capitalize org_name

--- build-sctg.py
+++ build-sctg.py
@@ -479,6 +479,7 @@ def sctgdesk_customization():
     RS_PUB_KEY = os.environ['RS_PUB_KEY']
     API_SERVER = os.environ['API_SERVER']
     ORG_NAME = os.environ['ORG_NAME']
+    ORG_NAME_CAPITALIZED = ORG_NAME.capitalize()
     TEAM_ID="HZF9JMC8YN"
     match = re.search(r'\(([A-Z0-9]+)\)$', MACOS_CODESIGN_IDENTITY)
     if match:
@@ -493,7 +494,7 @@ def sctgdesk_customization():
     replace_in_file('libs/portable/generate.py', 'rustdesk.exe', f'.{APP_NAME}.exe'.lower())
     replace_in_file('flatpak/rustdesk.json', 'rustdesk-', f'{APP_NAME}-'.lower())
     replace_in_file('flutter/lib/desktop/pages/desktop_setting_page.dart', 'https://rustdesk.com', f'https://{ORG_NAME}.eu.org'.lower())
-    replace_in_file('flutter/lib/desktop/pages/desktop_setting_page.dart', 'Purslane Ltd.', f'{ORG_NAME}, Purslane Ltd.')
+    replace_in_file('flutter/lib/desktop/pages/desktop_setting_page.dart', 'Purslane Ltd.', f'{ORG_NAME_CAPITALIZED} and Purslane Ltd.')
     replace_in_all_typed_files('ts', 'OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=', RS_PUB_KEY)
     replace_in_all_typed_files('ts', 'rs-ny.rustdesk.com', RENDEZVOUS_SERVER)
     replace_in_all_typed_files('ts', 'rs-sg.rustdesk.com', RENDEZVOUS_SERVER)
