826d15bf9f7d3ee96d074c98900fa85da43f8308
Ronan <ronan.le_meillat@highcanfly.club>
2024-04-19 21:22:40 +0200

    add sourcemap

--- flutter/web/js/vite.config.js
+++ flutter/web/js/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     build: {
+        sourcemap: true,
         manifest: false,
         rollupOptions: {
             output: {
